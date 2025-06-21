b87d985c363f27c4b7df433166606cfd42047ad6
Rodrigo Jaramillo <rodrigojaramilloga@gmail.com>
2025-06-21 16:56:32 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :ai_chats, only: [ :create ] do
+    resources :ai_chat_messages, only: [ :index, :create ]
+  end
+  get 'users/profile', to: 'user#profile'
+  get 'psychologists', to: 'users#index'
+  get 'psychologists/:id', to: 'users#show'
+  resources :schedules
+  resources :therapy_requests, only: [ :create ]
+  resources :appointments
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
