e6fa880b042079f2311f1e406628c152eb9dab90
Rodrigo Jaramillo <rodrigojaramilloga@gmail.com>
2025-06-25 17:03:42 -0500

    add request and delete request

--- app/controllers/therapy_requests_controller.rb
+++ app/controllers/therapy_requests_controller.rb
@@ -1,12 +1,23 @@
 class TherapyRequestsController < ApplicationController
   def create
     TherapyRequest.create(patient_id: current_user.id, psychologist_id: params[:psychologist_id], status: 0)
-    redirect_to root_path
+    redirect_to requests_path
   end
 
   def update
+    @request = TherapyRequest.find(params[:id])
+    @request.status = 2
+    @request.save
+    redirect_to requests_path
   end
 
   def destroy
+    @request = TherapyRequest.find(params[:id])
+    @request.destroy
+    redirect_to requests_path, status: :see_other
+  end
+
+  def requests
+    @requests = current_user.patient_therapy_requests
   end
 end
