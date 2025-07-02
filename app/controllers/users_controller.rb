7f59e5de60665a704505859607d6ba1248689960
Rodrigo Jaramillo <rodrigojaramilloga@gmail.com>
2025-07-01 20:32:19 -0500

    Chat with patient

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,22 @@ class UsersController < ApplicationController
   end
 
   def dashboard
-    
+    if current_user.psychologist?
+      @last_appointment = current_user.psychologist_appointments
+                                        .joins(:patient)
+                                        .order(updated_at: :desc).first
+    elsif current_user.patient?
+      @last_appointment = current_user.patient_appointments
+                                      .joins(:psychologist)
+                                      .order(updated_at: :desc).first
+    end
+
+  @messages = if @last_appointment
+                PsychologistMessage.where(appointment: @last_appointment).order(created_at: :asc)
+              else
+                []
+              end
+
+  @message = PsychologistMessage.new
   end
 end
